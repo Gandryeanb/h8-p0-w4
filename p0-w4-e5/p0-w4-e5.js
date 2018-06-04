bd3b4186b08613a3fff842327e4961101b8d3fae
gandryeanb <gstandrianb@gmail.com>
2018-06-05 02:42:33 +0700

    adding task e5

--- p0-w4-e5/p0-w4-e5.js
+++ p0-w4-e5/p0-w4-e5.js
@@ -1,5 +1,21 @@
 function ubahHuruf(kata) {
-  // you can only write your code here!
+  /* -membuat variable yg menampung alphabet
+   * -membuat variable yg menampung value hasil perubahan str kata
+   */ 
+  var alphabet = "abcdefghijklmnopqrstuvwxyza"
+  var store = "";
+  /* -membuat loop yg berfungsi sebagai proses memasukan alphabet
+   *  yang telah di proses dengan perulangan sesuai length
+   *  dari kata
+   */
+  for (var init = 0; init < kata.length; init++) {
+    /* variable alphaIndex tediri dari nilai index dari huruf kata
+     * yg mengikuti alur loop
+     */
+    var alphaIndex = alphabet.indexOf(kata[init]);
+    store = store + alphabet[alphaIndex + 1]
+  }
+  return store;
 }
 
 // TEST CASES
